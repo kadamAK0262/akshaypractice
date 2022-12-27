f71624bd603718e8b0742ff77ba8283eb32c750f
kadamAK0262 <akshaykadam0262@gmail.com>
2022-12-27 09:31:45 +0530

    main file

---
+++ main.py
@@ -0,0 +1,61 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+"""print("hello world")
+name = "akshay"
+age = 22
+print(name)
+print(age)
+
+first_name = "Tony"
+last_name = "Stark"
+age = 51
+is_genius = True
+print(first_name + " " + last_name)
+print(age)
+print(is_genius)
+
+name =input("what is your name")
+print("hello" + name)
+
+old_age = float(input(" what is your age:"))
+new_age = old_age + 4
+print(new_age)"""
+
+"""first = int(input("first number:"))
+second = int(input("second number:"))
+sum = first + second
+print("The sum is:" + str(sum))"""
+
+"""name  = "Akshay Kadam"
+print("a" in name)
+print(name.find('K'))
+print(3!=2)"""
+#pattern printing
+"""i =1
+while i<=5:
+    print(i*"*")
+    i=i+1"""
+#reverse pattern
+"""i=5
+while i>=0:
+    print(i*"*")
+    i=i-1"""
+
+"""students = ["Akshay","Pranit", "bhushan","Shubham","NItin","Mangesh"]
+
+for a in students:
+    if a == "Shubham":
+        break
+    print(a)"""
+
+"""students = ["Akshay","Pranit", "bhushan","Shubham","NItin","Mangesh"]
+
+for a in students:
+    if a == "Shubham":
+        continue
+    print(a)"""
+
